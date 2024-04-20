67ecfdc103946475f8579970db0697cb85da2966
souznerushimiu <zelnastik@555gmail.com>
2024-04-20 12:19:05 +1000

    sql1

---
+++ src/hometask22/test.java
@@ -0,0 +1,281 @@
+package hometask22;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+public class test {
+    public static void main(String[] args) {
+///////        boolean bool;
+//////        bool = foo1() || foo2();
+//////        System.out.print(" ");
+//////        bool =  foo1() && foo2();
+//////        System.out.print(" ");
+//////        bool = foo2() || foo1();
+//////        System.out.print(" ");
+//////        bool = foo2() && foo1();
+//////    }
+//////
+//////    static boolean foo1(){
+//////        System.out.print("foo1()");
+//////        return true;
+//////    }
+//////    static boolean foo2(){
+//////        System.out.print("foo2()");
+//////        return false;
+//////    }
+////
+//////        int i = 1, j = 1, k = 1;
+//////        if (i++ > 1 && j++ < 2 || k++ == 1) {
+//////            System.out.print(1);
+//////        } else {
+//////            System.out.print(2);
+//////        }
+//////
+//////        System.out.print(i);
+//////        System.out.print(j);
+//////        System.out.print(k);
+//////        System.out.println("Hello");
+//////        while(1)
+//////        {
+//////            System.out.print(" world");
+//////            break;
+//////        }
+//////
+//////        int i = 0;
+//////        int j = i++ + ++i;
+//////        System.out.println(j);
+////
+//////        try{
+//////            throw  new B();
+//////        }catch (A exception) {
+//////            A.print();
+//////        }catch (B exception) {
+//////            B.print();
+//////        }
+//////
+//////    }
+//////}
+////
+//////class A extends Exception{
+//////    static public void print(){
+//////        System.out.println("A got caught");
+//////    }
+//////}
+//////class B extends A{
+//////    static public void print(){
+//////        System.out.println("B got caught");
+//////    }
+//////}
+//////byte num = (byte) 127;
+//////num++;
+//////        System.out.println(num);
+////
+//////        int i = 10, j = 12, k = 1;
+//////        k+= i++ - --j;
+//////        System.out.println(k);
+////
+//////        boolean b1 = true, b2 = false, b3 = true;
+//////        System.out.println(b1 && b2 && !b3);
+//////
+//////        float num = 5f;
+//////        System.out.println("a is equal to " + num/0.0f);
+////
+//////        System.out.println("Main.main(String[] arr) is called");
+//////    }
+//////        public static void main() {
+//////            System.out.println("Main.main() is called");
+//////        }
+////
+//////String str1 = "Hello";
+//////String str2 = "Hello";
+//////if (str1 == str2)
+//////    System.out.println("Equal");
+//////else
+//////    System.out.println("Not");
+////        System.out.println(sum(1,2,3,4,5));
+////
+////    }
+//////    public static int sum(int ...y, int x){
+//////        int s = 0;
+//////        for (int i = 0; i < y.length; i++) {
+//////            s+=y[i];
+//////        }
+//////        return s + x;
+////        int num = 0;
+////        for (int i = 0; i < 100; i++)
+////            num = num++;
+////        System.out.println(num);
+////        Color c1 = new Color("Red");
+////        changeColor(c1);
+////        System.out.println(c1.getName());
+////
+////
+////    }
+////    public static void changeColor(Color c){
+////        c = new Color("Blue");
+////        System.out.print(c.getName());
+////    }
+////}
+////class Color{
+////    String name;
+////
+////    public Color(String name) {
+////        this.name = name;
+////    }
+////
+////    public String getName() {
+////        return name;
+////    }
+////
+////    public void setName(String name) {
+////        this.name = name;
+////    }
+//
+////        String str1 = "my test";
+////        String str2 = "this is";
+////        String str3;
+////
+////        switch (str1) {
+////            case "MY TEST": str3 = str1 + str2; break;
+////            case "MY Test": str3 = str2 + str1; break;
+////            case "my test": str3 = str1 + str1; break;
+////            default: str3 = str2 + str2;
+////
+////        }
+////        System.out.println(str3);
+////        String[] text = new String[]{
+////                "I ",
+////                "Love ",
+////                "Programming ",
+////                "Tests"
+////        };
+////        int i = 0;
+////        while (i < text.length){
+////            System.out.println(text[i]);
+////            i++;
+////        }
+////        float real = 0.0f/0.0f;
+////        if(real == real)
+////            System.out.println("Equal");
+////        else
+////            System.out.println("Not");
+//        MyMathClass m = new MyMathClass();
+//        int i = 10, j = 15;
+//        m.sum(i,j);
+//
+//        float f1 = 3.4f, f2 = 9.23f;
+//        m.sum(f1,f2);
+//
+//        char c1 = 'a', c2 = 'b';
+//        m.sum(c1,c2);
+
+
+//      int[] a = new int[]{2,4,6,8,10};
+//      int[] b = new int[a.length];
+//      for (int i = 0; i < a.length; i++) {
+//          b[i] = a[i] * 2;
+//          System.out.println(b[i]);
+//      }
+//      A var = new B();
+//      var.print();
+//      List listColors = new ArrayList<>();
+//      listColors.add("Red");
+//      listColors.add("Green");
+//      listColors.add("Blue");
+//
+//      changeMe(listColors);
+//      System.out.println(listColors);
+
+//        int i = 10, j = 12;
+//        i+= ++j;
+//        System.out.println(i);
+//        Hello hello = null;
+//        hello.print();
+//        boolean b = getValue(i, j) ? true : false;
+//        System.out.println(b);
+//    }
+//    public static boolean getValue(int i, int j){
+//        return i > j;
+//        A var = new B();
+//        var.print();
+//        int i = 5, j = 8, k = 0;
+//
+//        if(i > j && i++ < 10 && (k+= i + j)>10){
+//            System.out.println("true");
+//        }
+//        System.out.print(i);
+//        System.out.print(j);
+//        System.out.print(k);
+//        print("Hello");
+//    }
+//    void print(String str){
+//        System.out.println(str);
+//        print("Hello");
+//        System.out.println(2 + 2 == 5 && 12/4 ==3 || 2 == 5%3);
+
+//        int arr1[] = {1,2,3,4,5};
+//        int arr2[] = {1,2,3,4,5};
+//        if(arr1==arr2)
+//            System.out.println("equal");
+//        else
+//            System.out.println("not");
+//
+//    }
+        int i = 10;
+        int j = 25;
+        System.out.println(i + " " + j);
+    }
+
+//    void print(String str) {
+//        System.out.println(str);
+//    }
+}
+
+// class A{
+//    static public void print(){
+//        System.out.println("A.print is called");
+//    }
+// }
+//class B extends A{
+//    static public void print(){
+//        System.out.println("B.print is called");
+//    }
+//}
+//class Hello{
+//    public static void print(){
+//        System.out.println("Hello World!");
+//    }
+//}
+//  public static void changeMe(final List listColors){
+//      listColors.add("Cyan");
+//      listColors.remove("Blue");
+//  }
+//}
+
+//class A{
+//    public void print(){
+//        System.out.println("A.print() is called");
+//    }
+//}
+
+//class B extends A{
+//    public void print(){
+//        System.out.println("B.print() is called");
+//    }
+//}
+
+//class MyMathClass{
+//    public void sum(int i1, int i2){
+//        System.out.println(":int");
+//    }
+//    public void sum(String str1, String str2){
+//        System.out.println(":String");
+//    }
+//    public void sum(double d1, double d2){
+//        System.out.println(":double");
+//    }
+//
+//    int[] a = new int[]{2,4,6,8,10};
+//    int[] b = new int[a.length];
+//    for(int)
+//}
